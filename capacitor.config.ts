d81a3954ffba257a3807124eaf7a650958e60b63
Sasagayo29 <riquelmymiyasawaborges@gmail.com>
2026-07-19 19:34:52 -0300

    Commit...

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smartlist.app',
+  appName: 'SmartList',
+  webDir: 'dist/smart-list/browser'
+};
+
+export default config;
